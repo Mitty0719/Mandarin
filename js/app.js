ab0b54156464a6b6be4dc9709eff58fe6c01a530
Mitty0719 <sprout071900@gmail.com>
2022-10-29 21:25:07 +0900

    Test GeometryBox Rendering

--- js/app.js
+++ js/app.js
@@ -2,16 +2,47 @@ import * as THREE from 'three';
 
 class App{
   constructor(){
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color( 0x6495ED );
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    this.camera.position.set(100, 120, 150);
+    this.camera.lookAt(0, 0, 0);
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( this.renderer.domElement );
 
     const geometry = new THREE.BoxGeometry( 100, 100, 100 );
-    const material = new THREE.MeshBasicMaterial( { color: '#00ff00' } );
-    const cube = new THREE.Mesh ( geometry, material );
-    scene.add(cube);
+    const material = [
+      new THREE.MeshBasicMaterial( { color: 0x11ff00 } ),
+      new THREE.MeshBasicMaterial( { color: 0x0ff0f0 } ),
+      new THREE.MeshBasicMaterial( { color: 0x00ff0f } ),
+      new THREE.MeshBasicMaterial( { color: 0xffff00 } ),
+      new THREE.MeshBasicMaterial( { color: 0xf0ff00 } ),
+      new THREE.MeshBasicMaterial( { color: 0x00ffff } ),
+    ];
+    this.cube = new THREE.Mesh( geometry, material );
+
+    this.scene.add(this.cube);
+    this.renderer.render( this.scene, this.camera );
+
+    window.addEventListener('mousemove', this.moveMouse.bind(this));
+
+    requestAnimationFrame(this.render.bind(this));
+
+  }
+
+  moveMouse(e){
+    const cameraX = e.clientX - innerWidth / 2;
+    const cameraY = e.clientY - innerHeight / 2;
+    // this.camera.position.set(cameraX, cameraY, 300);
+    this.cube.position.set(cameraX, cameraY, 0)
+  }
+
+  render(){
+    window.requestAnimationFrame(this.render.bind(this));
+
+    this.renderer.render( this.scene, this.camera );
   }
 }
 
